b65ebd83b111d3aae2d4c18c51c314834a463d69
sarvinduri <s.ramm.s.ramm@gmail.com>
2023-05-18 10:07:53 +0300

    Добавьте файлы проекта.

---
+++ Glazkova5/Classes/ClassSpors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glazkova5.Classes
+{
+    internal class ClassSpors
+    {
+        string fullname;
+        string place;
+
+        public string FullName { get { return fullname; } set { fullname = value; } }
+        public string Place { get { return place; } set { place = value; } }
+
+        public ClassSpors()
+        {
+            fullname= string.Empty;
+            place= string.Empty;
+        }
+        public ClassSpors(string fname, string pl)
+        {
+            FullName = fname;
+            Place = pl;
+        }
+    }
+}
